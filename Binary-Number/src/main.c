470d2c9fbb0495e6d01d750aa678fdbe835ceba7
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-23 21:42:33 +0500

    Adding MAIN | Binary Number

---
+++ Binary-Number/src/main.c
@@ -0,0 +1,21 @@
+/*
+Developer && Owner: МогучийДД (MoguchiyDD)
+LICENSE: MIT License which is located in the text file LICENSE
+
+Goal: Write The BINARY NUMBER from 0
+Result: Complete The BINARY NUMBER from 0
+
+Past Modification:
+Last Modification: Adding DOCUMENTATION
+Modification Date: 2024.03.23, 09:35 PM
+
+Create Date: 2024.03.23, 09:26 PM
+*/
+
+
+#include <stdio.h>
+
+int main() {
+  printf("Binary Number\n");
+  return 0;
+}

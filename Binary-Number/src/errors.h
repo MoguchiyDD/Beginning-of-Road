185d9479c939ab0865f8b97556b083789bc18d28
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-24 00:29:50 +0500

    Adding ERRORS | Binary Number

---
+++ Binary-Number/src/errors.h
@@ -0,0 +1,26 @@
+/*
+Developer && Owner: МогучийДД (MoguchiyDD)
+LICENSE: MIT License which is located in the text file LICENSE
+
+Goal: Write your own ERRORS for The Program
+Result: Complete your own ERRORS for The Program
+
+Past Modification: Adding The «COUNT_ARGUMENTS» BLOCK ERRORS
+Last Modification: Adding The «COMMAND_IS_MISSING» BLOCK ERRORS
+Modification Date: 2024.03.24, 12:22 AM
+
+Create Date: 2024.03.23, 10:52 PM
+*/
+
+
+#ifndef ERRORS
+  #define ERRORS
+
+  // Number of Arguments from Terminal
+  #define D_COUNT_ARGUMENTS 1
+  #define S_COUNT_ARGUMENTS "NumArgsError"
+
+  // Command is Missing
+  #define D_COMMAND_IS_MISSING 2
+  #define S_COMMAND_IS_MISSING "CmdMissError"
+#endif

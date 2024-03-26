6981608393cfbed1ece9d46562365cbebf76408f
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-26 17:51:51 +0500

    Update DEFINE | Binary Number

--- Binary-Number/src/binary/binary.h
+++ Binary-Number/src/binary/binary.h
@@ -7,14 +7,14 @@ Result: Complete your own BINARY NUMBER for The Program
 
 Past Modification: Adding DOCUMENTATION
 Last Modification: Adding The «10->2» FUNCTION
-Modification Date: 2024.03.24, 01:58 PM
+Modification Date: 2024.03.24, 05:13 PM
 
 Create Date: 2024.03.24, 01:56 PM
 */
 
 
-#ifndef BINARY_FILE
-  #define BINARY_FILE
+#ifndef BINARY_FILE_H
+  #define BINARY_FILE_H
 
   char* db(char *number);
 #endif

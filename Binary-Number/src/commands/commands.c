0f746b93416327463070ad4dfdbf13f2c310d6b4
MoguchiyDD <ket-ket-01@bk.ru>
2024-03-31 23:26:13 +0500

    Adding «OD», «DO», «HD», «DH», «OH» and «HO» | Binary Number

--- Binary-Number/src/commands/commands.c
+++ Binary-Number/src/commands/commands.c
@@ -5,9 +5,9 @@ LICENSE: MIT License which is located in the text file LICENSE
 Goal: Declare STRING COMMANDS in NUMERIC Form
 Result: STRING COMMANDS to NUMERIC COMMANDS
 
-Past Modification: Adding COPYRIGHT
-Last Modification: Adding The «int_cmd_args» FUNCTION
-Modification Date: 2024.03.28, 10:27 PM
+Past Modification: Adding The «int_cmd_args» FUNCTION
+Last Modification: Adding The «OD», «DO», «HD», «DH», «OH» and «HO» COMMANDS
+Modification Date: 2024.03.31, 11:06 PM
 
 Create Date: 2024.03.28, 09:50 PM
 */
@@ -26,7 +26,10 @@ Create Date: 2024.03.28, 09:50 PM
   } cmds[COMMANDS] = {
     {"-bd\0", BD}, {"-db\0", DB},
     {"-bo\0", BO}, {"-ob\0", OB},
-    {"-bh\0", BH}, {"-hb\0", HB}
+    {"-bh\0", BH}, {"-hb\0", HB},
+    {"-od\0", OD}, {"-do\0", DO},
+    {"-hd\0", HD}, {"-dh\0", DH},
+    {"-oh\0", OH}, {"-ho\0", HO}
   };
 #endif
 

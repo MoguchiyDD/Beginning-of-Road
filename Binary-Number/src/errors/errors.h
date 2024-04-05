33c1fb857f0177c1a2542c3ab4c44006dfe974b7
MoguchiyDD <ket-ket-01@bk.ru>
2024-04-05 22:56:42 +0500

    Adding FUNCTIONS for ERRORS | Binary Number

--- Binary-Number/src/errors/errors.h
+++ Binary-Number/src/errors/errors.h
@@ -5,13 +5,15 @@ LICENSE: MIT License which is located in the text file LICENSE
 Goal: Write your own ERRORS for The Program
 Result: Complete your own ERRORS for The Program
 
-Past Modification: Adding The «BINARY» BLOCK ERRORS
-Last Modification: Adding The «MAXIMUM_INTEGER» BLOCK ERRORS
-Modification Date: 2024.03.29, 06:28 PM
+Past Modification: Adding The «MAXIMUM_INTEGER» BLOCK ERRORS
+Last Modification: Adding The BLOCKS ERRORS FUNCTIONS
+Modification Date: 2024.04.05, 10:11 PM
 
 Create Date: 2024.03.23, 10:52 PM
 */
 
+
+#include <stdint.h>
 
 #ifndef ERRORS
   #define ERRORS
@@ -51,4 +53,11 @@ Create Date: 2024.03.23, 10:52 PM
   // Hexdecimal
   #define D_HEXDECIMAL 9
   #define S_HEXDECIMAL "HexdecimalError"
+
+  void num_args_error(int argc);  // 1
+  void command_is_missing();  // 2
+  void memory_full_error();  // 3
+  void maximum_integer_error();  // 4
+  void binary_search_error();  // 5
+  void bodh(uint_least8_t integer_error, char *string_error, char *from, char *to, uint_least8_t max_bodh, char *copy);  // 6, 7, 8, 9
 #endif
